ae7119ec3dc1f0b6e0722792ca0064d7c0d6b549
David Oesch <david.oesch@gmail.com>
2024-12-17 12:49:40 +0100

    fix link in map

--- app.py
+++ app.py
@@ -187,8 +187,8 @@ def create_map(center, zoom):
         zoom_start=zoom,
         control_scale=True,
         tiles="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
-        attr='Map data: &copy; <a href="https://www.swisstopo.ch">swisstopo</a>)',  # noqa: E501
-    )
+        attr='Map data: &copy; <a href="https://www.swisstopo.ch" target="_blank" rel="noopener noreferrer">swisstopo</a>, <a href="https://www.housing-stat.ch/" target="_blank" rel="noopener noreferrer">BFS</a>',
+        )
     Draw(
         export=False,
         position="topleft",
