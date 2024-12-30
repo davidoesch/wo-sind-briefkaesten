e5b8378e8bb7ba2e542b3279df6cf430569c185e
David Oesch <david.oesch@gmail.com>
2024-12-30 20:42:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # List of Streamlit App URLs
 
 STREAMLIT_APPS = [
-    "https://wieviele-briefkaesten-gibt-es.streamlit.app//"
+    "https://wieviele-briefkaesten-gibt-es.streamlit.app/",
+    "https://prettymapp-igcwnd1htw.streamlit.app/"
 ]
